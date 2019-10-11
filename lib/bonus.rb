9873e458fe86238d8f1b7d157191b75129e4880e
Keillan Cruickshank <cruickshankxkeillan90@gmail.com>
2019-10-11 20:52:35 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -3,7 +3,7 @@ def bonus
   puts epic_tragedy
 
   epic_tragedy = {
-    
+
    :montague => {
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
@@ -22,7 +22,7 @@ def bonus
           {name: "Nurse", age: "44", attitude: "worried"}
       ]
    }
-   
+
 }
 
   #code your solution here: "Montel Willaims"
