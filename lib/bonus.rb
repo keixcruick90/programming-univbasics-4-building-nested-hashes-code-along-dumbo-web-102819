0fbca1b2c69675b9414d4e641c37f87c6916a62e
Keillan Cruickshank <cruickshankxkeillan90@gmail.com>
2019-10-11 21:00:05 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -1,10 +1,6 @@
 def bonus
   epic_tragedy[:montague][:patriarch][:name] = "Michael Jordan"
-
-puts epic_tragedy
-
-#  =>
-{
+  puts epic_tragedy = {
    :montague => {
       :patriarch => {name: "Michael Jordan", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
